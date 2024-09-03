a58929b8b537b1aadf7c5ead66c8761066b4761a
ricsaittan <ricky.tan@edu.sait.ca>
2024-09-03 15:19:09 -0600

    Week 2 Components Added

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Ricky Tan</h1>
+            <Link href = "https://github.com/ricsaittan">https://github.com/ricsaittan</Link>
+        </main>
+    );
+}
